d7cb432aac201949cf63ccaed5353a9a5e379100
Liron Shahar-Atia <liron.shaharatia@gmail.com>
2019-11-27 22:43:04 +0200

    Img & Minimal design

--- frontend/src/views/ItemDetails.vue
+++ frontend/src/views/ItemDetails.vue
@@ -1,7 +1,32 @@
 <template>
     <section class="item-details">
         <h1>item-details - </h1>
-        <pre>{{this.item}}</pre>
+         <li class="preview-container item-preview">
+            <!-- <section > -->
+                <h2>{{item.name}}</h2>
+                <img class="img-details" :src="item.imgUrl">
+                <h3>Price: ${{item.price}}</h3>
+                <h3>Category: ${{item.category}}</h3>
+                <h3>Publishe at: ${{item.createdAt}}</h3>
+                <h3>Condition: ${{item.conditionS}}</h3>
+                <h3>Status: ${{item.status}}</h3>
+                <h3>Seller: {{item.owner.name}}</h3> 
+                <h3>{{item.owner.imgUrl}}</h3>
+                <h3>Additional information: {{item.description}}</h3>
+
+                <button>like</button>
+                <!-- <button @click="addToWishList(item._id)">like</button> -->
+                <!-- <button >Details</button> -->
+                <!-- <button @click="itemDetails(item._id)"> -->
+
+                <!-- <router-link :to="'/item/'+item._id">Details</router-link> -->
+                <!-- </button> -->
+            <!-- </section> -->
+        </li>
+        
+        
+        
+        <!-- <pre>{{this.item}}</pre> -->
     </section>
 </template>
 
