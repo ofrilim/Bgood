f42895ced27dbf4f9f6e3b3a2a0d707732c0cab3
Hila S <hilas60@gmail.com>
2019-11-28 11:30:03 +0200

    added functionality to user store

--- frontend/src/views/UserDetails.vue
+++ frontend/src/views/UserDetails.vue
@@ -5,8 +5,20 @@
 </template>
 
 <script>
+import store from '../store/index.js'
+
 export default {
     name: 'user-details',
-    
+    store: store,
+    computed:{
+        user(){
+            return this.$store.getters.user
+        }
+    },
+    created(){
+        const userId = this.$route.params.id
+        this.$store.dispatch({type: 'loadUser', userId})
+        // this.$store.getters('user')
+    }
 }
 </script>
