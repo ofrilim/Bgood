ac26da39912a48bc8d32795e3be26f5fdfb54d49
Limor Ofri <limorofri7@gmail.com>
2019-12-02 16:14:34 +0200

    commitinggg

--- frontend/src/services/HttpService.js
+++ frontend/src/services/HttpService.js
@@ -1,6 +1,5 @@
 import router from '@/router'
 import Axios from 'axios';
-// import store from '@/store'
 
 const BASE_URL = process.env.NODE_ENV === 'production'
     ? '/'
@@ -12,7 +11,6 @@ var axios = Axios.create({
 
 export default {
     get(endpoint, data){
-        // console.log('IN HTTPSERVICE GET FUNCTION',endpoint, data)
         return ajax(endpoint, 'GET', data)
     },
     post(endpoint, data){
@@ -42,45 +40,3 @@ async function ajax(endpoint, method='get', data=null) {
         }
     }
 }
-
-// import axios from 'axios'
-// import router from '../router/index.js'
-
-// function _handleError(err) {
-//     console.log('Err:', err);
-//     if (err.response.status === 401) {
-//         sessionStorage.clear();
-//         router.push('/');
-//     }
-//     throw err;
-// }
-
-// function get(url) {
-//     return axios.get(url)
-//         .then(res => res.data)
-//         .catch(_handleError)
-// }
-
-// function remove(url) {
-//     return axios.delete(url)
-//         .catch(_handleError)
-// }
-
-// function post(url, data) {
-//     return axios.post(url, data)
-//         .then(res => res.data)
-//         .catch(_handleError)
-// }
-// function put(url, data) {
-//     return axios.put(url, data)
-//         .then(res => res.data)
-//         .catch(_handleError)
-// }
-
-
-// export const HttpService = {
-//     get,
-//     delete: remove,
-//     post,
-//     put
-// } 
