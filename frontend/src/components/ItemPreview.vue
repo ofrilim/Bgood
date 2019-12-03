1028eb246840f9d248fa240b3b0c8865ddd8fefb
Limor Ofri <limorofri7@gmail.com>
2019-12-03 20:20:45 +0200

    after design by sasssi!

--- frontend/src/components/ItemPreview.vue
+++ frontend/src/components/ItemPreview.vue
@@ -1,24 +1,24 @@
 <template>
-    <section>
-        <li class="preview">
-            <router-link :to="`/item/${item._id}`">
-                <i class="fa fa-heart preview-heart" @click.stop="addToWishList(item._id)"></i>
+    <li class="preview">
+        <router-link :to="`/item/${item._id}`">
+            <i class="fa fa-heart preview-heart" @click.stop="addToWishList(item._id)"></i>
+            <div class="img-hover">
                 <img class="img-preview" :src="item.imgUrl"/>
-            </router-link>
-            <div class="preview-content flex flex-col">
-                <h2 class="preview-name bold">{{item.name}}</h2>
-                <div class="flex flex-between">
-                    <h2>Price:  {{item.price}} $</h2>
-                </div>
-                <router-link :to="`/user/${item.owner._id}`">
-                <div class="flex align-center">
-                    <img class="avatar-img pointer" :src="item.owner.imgUrl"/><small class="by">By: {{item.owner.name}}</small>
-                </div>
-                </router-link>
-            <slot></slot>
             </div>
-        </li>
-    </section>
+        </router-link>
+        <div class="preview-content flex flex-col flex-around">
+            <h2 class="preview-name bold">{{item.name}}</h2>
+            <div class="preview-price flex flex-between">
+                <h2>Price:  {{item.price}} $</h2>
+            </div>
+            <router-link :to="`/user/${item.owner._id}`">
+            <div class="flex align-center">
+                <img class="avatar-img pointer" :src="item.owner.imgUrl"/><small class="by">By: {{item.owner.name}}</small>
+            </div>
+            </router-link>
+        <slot></slot>
+        </div>
+    </li>
 </template>
 
 
