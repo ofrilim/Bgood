484b0f56184d2eca752da6b3b46404a634e43d4d
Liron Shahar-Atia <liron.shaharatia@gmail.com>
2019-12-07 04:11:07 +0200

    About

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,10 +1,43 @@
 <template>
   <section class="about">
-    <h1>This is an about page</h1>
+    <section>
+    <!-- <h1>This is an about page</h1> -->
+      <span class="about-header">dressing well and being eco-friendly simultaneously – This is the essence of B-GOOD!</span><br><br>
+      1. <span class="bold center">You can afford more and better stuff</span>, using the same amount you planned to spend.<br><br>
+      2. <span class="bold">You help prevent environmental devastation</span><br>
+      Consider the amount of recourses needed for the production of a new piece of clothing.
+      – If it’s made from natural fibers, such as cotton, the land, water.
+      – If the item is made with artificial fibers are made with coal and petrol, which makes this clothing unsustainable. 
+      – Textile dyeing causes air and water pollution, and it affects the problem of worldwide water shortage.<br><br>
+      3. <span class="bold">You reduce the size of landfills</span><br>
+      landfills around the world are flooded with barely worn clothing, which leads to environmental issues with the soil, wildlife, and the sheer landmass.<br><br>
+      4. <span class="bold">You can add unique pieces to your fashion arsenal</span><br>
+      You can find gorgeous pieces that fit into whatever is currently trendy, yet different enough to stand out from the crowd.<br><br>
+      5. <span class="bold">You won’t fund slave or child labor</span><br>
+      Fashion is frequently using unsafe and unfair labor.The less we buy - the less people and children will be hurt.<br><br>
+      6. <span class="bold">You support local economy and creativity</span><br>
   </section>
+
+  <section class="flex align-end">
+    <div class="about-admin">
+      <img class="about-img" src="https://ca.slack-edge.com/TMCJAUBPU-UMUV8ULF7-a9423eb038b6-512" alt="">
+      <h1>Hila Solomon</h1>
+    </div>
+    <div class="about-admin">
+      <img class="about-img" src="https://ca.slack-edge.com/TMCJAUBPU-UMHAG22AW-6fbebf3eeb8c-72" alt="">
+      <h1>Limor Ofri</h1>
+    </div>
+    <div class="about-admin">
+      <img class="about-img" src="http://res.cloudinary.com/dtkrff3dw/image/upload/v1575536234/e1kizodw5wtdslqgzsc4.jpg" alt="">
+      <h1>Liron Shahar-Atia</h1>
+    </div>
+  </section>
+  </section>
+
 </template>
 
 
+http://contrabond.com/en/6-reasons-second-hand-clothes-better/
 
 
 
